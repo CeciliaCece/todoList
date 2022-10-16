3e4775730926fd7548a3f1dc4ad1a6799446303e
Cecilia <Cecilia222202@gmail.com>
2022-10-16 17:41:09 +0800

    新增環境

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,112 @@
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 const path = require('path');
+const glob = require('glob');
+
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  entry: './src/index.js',
+ 
+  entry: './src/js/index.js',
+ 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'js/index.js',
+    clean: true,
+    
   },
+
+  // Define development options
+
+  devtool: devMode ? "eval-cheap-module-source-map" : "source-map",
+
+  // Define used plugins
+  plugins: [
+    new Dotenv(),
+    new miniCssExtractPlugin({
+      filename:'main.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/img",
+          to: "img",
+        }
+      ]
+    }),],
+    
   module: {
     rules: [
+      // Use babel for JS files
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ]
+          }
+        }
+      },
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.s[ac]ss$/,
         use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
+          devMode ? "style-loader" : miniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
         ],
       },
+      // File loader for images
+      {
+        test: /\.(jpg|jpeg|png)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      },
+      // File loader for svg
+      {
+        test: /\.(svg)$/i,
+        loader: 'svg-inline-loader',
+      }
     ],
   },
+  
+  // Configure the "webpack-dev-server" plugin
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
     compress: true,
     port: 9000,
-    open:true
+    open: true
   },
+  
+  // Performance configuration
+  performance: {
+    hints: false
+  }
 };
